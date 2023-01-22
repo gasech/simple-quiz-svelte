fc6b37f0775524623f49966c91ea80a7e25f1868
Gabriel <80397001+gasech@users.noreply.github.com>
2023-01-21 21:11:56 -0300

    Removed unused CSS selectors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <svelte:head>
-<title>Home</title>
+	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
@@ -30,21 +30,5 @@
 
 	h1 {
 		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
